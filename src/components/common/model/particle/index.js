c7872891ee104e9e8ec0d3ce53f563fc76b28192
eastcopper <ldh7228@gmail.com>
2022-03-09 02:03:26 +0900

    ⚡ :: (#18)[Particle] Add Comments

--- src/components/common/model/particle/index.js
+++ src/components/common/model/particle/index.js
@@ -6,31 +6,31 @@ export function ParticleGroup() {
   const particleMesh = useRef(null);
   useFrame(() => {
     particleMesh.current.rotation.z = particleMesh.current.rotation.y += 0.002;
-  });
-  return <group ref={particleMesh}>{Particles()}</group>;
+  }); // 모든 파티클의 애니메이션
+  return <group ref={particleMesh}>{Particles()}</group>; // 파티클을 그룹으로 만들어 생성
 }
 
 function Particles() {
   let result = [];
   for (let i = 0; i < 125; i++) {
     result.push(<Fregment />);
-  }
+  } // 125개의 fregent를 배열에 삽입
   return result;
 }
 
 function Fregment() {
-  let scale = getRandomIntInclusive(5, 20) / 300;
+  let scale = getRandomIntInclusive(5, 20) / 300; // 파티클 크기 랜덤
   return (
     <mesh
       position={[
         getRandomIntInclusive(-30, 30),
         getRandomIntInclusive(-30, 30),
         getRandomIntInclusive(-30, 30),
-      ]}
+      ]} // 파티클 위치 랜덤
       scale={[scale, scale, scale]}
     >
       <tetrahedronGeometry args={[2, 0]} />
       <meshPhongMaterial attach="material" color="white" />
     </mesh>
-  );
+  ); // 파티클 하나 생성
 }
