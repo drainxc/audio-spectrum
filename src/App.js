c8a7c31fdf9ba39ad27cd1ee2217126356d78a37
eastcopper <ldh7228@gmail.com>
2022-03-09 03:22:55 +0900

    ⚡ :: (#23)[App] Add random BackGround

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import MainPage from "./components/page/mainPage";
 import GlobalStyle from "./styles/styles";
+import { color } from "./lib/export/data";
+import { getRandomIntInclusive } from "./lib/function/random";
 
 function App() {
+  const colorNumber = getRandomIntInclusive(0, color.length - 1);
   return (
     <>
-      <GlobalStyle firstColor="#ee9ca7" lastColor="#ffdde1"/>
+      <GlobalStyle firstColor={color[colorNumber][0]} lastColor={color[colorNumber][1]} />
       <MainPage />
     </>
   );
